fc019c2d3fec7542eeed94a1be7bff3d74c84dd6
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2024-08-01 21:49:25 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -16,7 +16,7 @@ sudo apt build-dep -y linux
 cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source and patch
-git clone -b sm8250/v6.10 https://github.com/yuweiyuan8/sm8250-mainline.git --depth=1
+git clone -b sm8250/v6.10 https://github.com/yuweiyuan8/sm8250-mainline.git linux --depth=1
 cd linux  || exit
 
 # add some patch
