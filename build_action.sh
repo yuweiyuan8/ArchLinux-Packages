6bc472e6c7f838c72833161db24fb266048d40b8
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2025-03-10 09:40:59 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -16,7 +16,7 @@ sudo apt build-dep -y linux
 cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source and patch
-git clone -b lmi-mainline-v6.12.0rc6 https://github.com/yuweiyuan8/linux linux --depth=1
+git clone -b nikroks/alioth https://github.com/mainlining/linux.git linux --depth=1
 cd linux  || exit
 
 # add some patch
@@ -27,7 +27,7 @@ cd linux  || exit
 # sed -i "s/^.*CONFIG_DRM_PANEL_JDI_NT35596S.*$/CONFIG_DRM_PANEL_JDI_NT35596S=y/" .config
 
 # generate .config
-make ARCH=arm64 lmi_defconfig
+make ARCH=arm64 sm8250.config
 
 # build deb packages
 make -j$(nproc) ARCH=arm64 KBUILD_DEBARCH=arm64 KDEB_CHANGELOG_DIST=mobile CROSS_COMPILE=aarch64-linux-gnu- deb-pkg
