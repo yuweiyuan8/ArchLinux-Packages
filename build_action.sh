f73233d4b613a2d070c509377655af0ae41d8cdd
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2025-05-10 10:57:39 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -9,10 +9,7 @@ sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.
 # install dep
 sudo apt update
 sudo apt install -y wget 
-sudo apt install -y build-essential openssl pkg-config libssl-dev libncurses5-dev pkg-config minizip libelf-dev flex bison  libc6-dev libidn11-dev rsync bc liblz4-tool  
-sudo apt install -y gcc-aarch64-linux-gnu dpkg-dev dpkg git debhelper
-sudo apt build-dep -y linux
-
+sudo apt update && sudo apt install -y bc bison flex gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf make libelf-dev libssl-dev debhelper-compat debhelper build-essential device-tree-compiler
 # change dir to workplace
 cd "${GITHUB_WORKSPACE}" || exit
 
@@ -27,12 +24,12 @@ cd linux  || exit
 # add mix2s panel driver
 # sed -i "s/^.*CONFIG_DRM_PANEL_JDI_NT35596S.*$/CONFIG_DRM_PANEL_JDI_NT35596S=y/" .config
 
-# generate .config
-make ARCH=arm64 defconfig sm8250.config
-
+#Run mv scripts/package/mkdeb scripts/package/mkdebian -v
+mv scripts/package/mkdeb scripts/package/mkdebian -v
 # build deb packages
-make -j$(nproc) ARCH=arm64 KBUILD_DEBARCH=arm64 KDEB_CHANGELOG_DIST=mobile CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
-# This will generate several deb files in ../
+export PATH="/usr/lib/ccache:/usr/local/opt/ccache/libexec:$PATH"
+make -j16 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnuabeihf- defconfig sm8250.config bindeb-pkg
+
 
 # move deb packages to artifact dir
 cd ..
